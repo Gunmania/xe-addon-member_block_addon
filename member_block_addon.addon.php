995194c56282bcbbfca757677e622119d88ca83c
Gunmania <d.gunmania@gmail.com>
2015-01-13 21:16:13 +0900

    쪽지 페이지 지원

--- member_block_addon.addon.php
+++ member_block_addon.addon.php
@@ -28,4 +28,31 @@
 					echo '<script>window.location.href = "'.getUrl().'";</script>';
 			}
 		}
+
+		if(Context::get('message_list')) {
+			$msg_list = Context::get('message_list');
+			$logged_info = Context::get('logged_info');
+			$blocked_list = explode(",",$logged_info->block_list);
+			foreach($msg_list as $key => $val) {
+				if(in_array($val->nick_name, $blocked_list))
+					unset($msg_list[$key]);
+			}
+			Context::set('message_list',$msg_list);
+		}
+
+		if(Context::get('message_srl')) {
+			$msg_srl = Context::get('message_srl');
+			$logged_info = Context::get('logged_info');
+			$blocked_list = explode(",",$logged_info->block_list);
+			$oCommunicationModel = &getModel('communication');			
+			$oMessage = $oCommunicationModel->getSelectedMessage($msg_srl);
+			if(in_array($oMessage->nick_name, $blocked_list)) {
+				header("Content-Type: text/html; charset=UTF-8");
+				echo '<script>alert("차단한 회원이 보낸 쪽지입니다.");</script>';
+				if($_SERVER['HTTP_REFERER'])
+					echo '<script>window.location.href = "'.$_SERVER['HTTP_REFERER'].'";</script>';
+				else
+					echo '<script>window.location.href = "'.getUrl().'";</script>';
+			}
+		}
 	}
